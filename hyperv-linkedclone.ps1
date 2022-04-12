fc88b85adfebd8b9a3785f1191519c5a9544fc36
brian-anderson01 <54916029+brian-anderson01@users.noreply.github.com>
2022-04-12 15:35:19 -0400

    Update hyperv-linkedclone.ps1

--- hyperv-linkedclone.ps1
+++ hyperv-linkedclone.ps1
@@ -1,7 +1,6 @@
 ﻿$parentvm = Read-Host "Input the name of the parent VM"
 $childhvd = Read-Host "Input the full path and name of the child vhdx"
 $linkedname = Read-Host "Input a name for the new VM"
-#$linkedram = Read-Host "Input the ram amont for the new VM in GB"
 
 $parentvhd = Get-VM -VMName $parentvm | Select-Object -Property VMId | Get-VHD | Select "Path" -ExpandProperty "Path" | Out-String
 

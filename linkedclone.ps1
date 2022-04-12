6d09659cd5c5c256ab4dd46b0cf8548dc43e8269
brian-anderson01 <54916029+brian-anderson01@users.noreply.github.com>
2022-04-12 15:34:48 -0400

    Add files via upload

---
+++ linkedclone.ps1
@@ -0,0 +1,11 @@
+﻿$parentvm = Read-Host "Input the name of the parent VM"
+$childhvd = Read-Host "Input the full path and name of the child vhdx"
+$linkedname = Read-Host "Input a name for the new VM"
+#$linkedram = Read-Host "Input the ram amont for the new VM in GB"
+
+$parentvhd = Get-VM -VMName $parentvm | Select-Object -Property VMId | Get-VHD | Select "Path" -ExpandProperty "Path" | Out-String
+
+New-VHD -ParentPath $parentvhd -Path $childhvd -Differencing
+New-VM -Name $linkedname -MemoryStartupBytes 2GB -VHDPath $childhvd
+
+Write-Host "$linkedname was created sucessfully"
